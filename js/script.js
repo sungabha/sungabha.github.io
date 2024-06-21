0dd41e491f189f8837b192ade4404cb11dc6fb1e
Prof. - Xadk3! <xadkeg@duck.com>
2024-06-21 09:13:39 +0545

    .

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
+
+const onConfirmRefresh = function (event) {
+  event.preventDefault();
+  // TODO: Refresh via fetch API call.
+  return event.returnValue = "Are you sure? You want to exit this page?";
+}
+
+window.addEventListener("beforeunload", onConfirmRefresh, { capture: true });
